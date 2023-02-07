6265ab28c4714070a0bafb96624db1269e68a571
Vitalii <vitalii.nektony@gmail.com>
2023-02-08 00:30:30 +0200

    Task about apples and students

--- task_1.py
+++ task_1.py
@@ -1,5 +1,5 @@
 student = 30
 apples = 167
 
-print ('Apples per student:', apples // student)
-print ('Apples in a basket:', apples % student)
+print('Apples per student:', apples // student)
+print('Apples in a basket:', apples % student)
