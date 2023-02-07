2bbef36f1e746eca97a4b8880568bb639171fb45
Vitalii <vitalii.nektony@gmail.com>
2023-02-08 01:05:39 +0200

    Task about apples and students

--- task_1.py
+++ task_1.py
@@ -1,5 +1,5 @@
 n = int(input("Enter num of students: "))
 k = int(input("Enter num of apples: "))
 
-print('Apples per student:', new_value2 // new_value1)
-print('Apples in a basket:', new_value2 % new_value1)
+print('Apples per student:', k // n)
+print('Apples in a basket:', k % n)
