7f3ca0e8c0487a26e4127055b3930fa4f72eb1c4
Vitalii <vitalii.nektony@gmail.com>
2023-02-13 23:22:25 +0200

    Task about finding all characters in string

--- task_07.py
+++ task_07.py
@@ -1,15 +1,16 @@
 s = input("Enter text:")
-ch = input("Enter symbol:")
+ch = input("Enter character:")
 
-indexesString = ""
-for i in range(len(s)):
-    if s[i] == ch:
-        if len(indexesString) > 0:
-            indexesString += f", {i}"
-        else:
-            indexesString = f"{i}"
+index = -1
+is_found = False
+while True:
+    index = s.find(ch, index + 1)
+    if index == -1:
+        break
+    print(f"Found at index {index}")
+    is_found = True
 
-print(f"Symbol of '{ch}' in '{s}': {indexesString}")
-
+if not is_found:
+    print(f"The character '{ch}' was not found in '{s}'")
 
 
